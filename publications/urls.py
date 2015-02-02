f6e6282b551182b9767b3c70d0efe0daaaf52022
Maarten van Gompel <proycon@anaproy.nl>
2015-02-02 11:33:44 +0100

    Rewrote template urls to use {% url %}

--- publications/urls.py
+++ publications/urls.py
@@ -8,11 +8,11 @@ except ImportError:
 	from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-	url(r'^$', 'publications.views.year'),
-	url(r'^(?P<publication_id>\d+)/$', 'publications.views.id'),
-	url(r'^year/(?P<year>\d+)/$', 'publications.views.year'),
-	url(r'^tag/(?P<keyword>.+)/$', 'publications.views.keyword'),
-	url(r'^list/(?P<list>.+)/$', 'publications.views.list'),
-	url(r'^unapi/$', 'publications.views.unapi'),
-	url(r'^(?P<name>.+)/$', 'publications.views.person'),
+	url(r'^$', 'publications.views.year', name='index'),
+	url(r'^(?P<publication_id>\d+)/$', 'publications.views.id',name='publication'),
+	url(r'^year/(?P<year>\d+)/$', 'publications.views.year',name='year'),
+	url(r'^tag/(?P<keyword>.+)/$', 'publications.views.keyword', name='keyword'),
+	url(r'^list/(?P<list>.+)/$', 'publications.views.list',name='list'),
+	url(r'^unapi/$', 'publications.views.unapi',name='unapi'),
+	url(r'^(?P<name>.+)/$', 'publications.views.person', name='person'),
 )
